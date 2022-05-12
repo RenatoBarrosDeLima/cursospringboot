b2c9bba1cca814362ed68fe5a9200c9b63f81ae0
Renato Barros <renatobarros1992@hotmail.com>
2022-05-12 19:55:21 -0300

    Listando todas categorias com DTO

--- src/main/java/com/renatobarros/cursomc/services/CategoriaService.java
+++ src/main/java/com/renatobarros/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.renatobarros.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,11 +38,16 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-			
-		repo.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
+
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que tem produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+
 	}
 
 }
