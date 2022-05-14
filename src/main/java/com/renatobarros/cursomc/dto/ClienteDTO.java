a1024af8bca9d9a116e85fc1de7ebfd881b8706d
Renato Barros <renatobarros1992@hotmail.com>
2022-05-14 13:32:49 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/renatobarros/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/renatobarros/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.renatobarros.cursomc.domain.Cliente;
+import com.renatobarros.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
---
+++ src/main/java/com/renatobarros/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,54 @@
+package com.renatobarros.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.renatobarros.cursomc.domain.Cliente;
+import com.renatobarros.cursomc.dto.ClienteDTO;
+import com.renatobarros.cursomc.repositories.ClienteRepository;
+import com.renatobarros.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente cli = repo.findByEmail(objDto.getEmail());
+		if(cli != null && !cli.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
